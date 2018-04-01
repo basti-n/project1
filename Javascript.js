2963f43d60e76cb1e43ff9270ade2d851c8af720
basti <sebastian.neumair@gmail.com>
2018-04-01 09:18:47 +0200

    enter

--- Javascript.js
+++ Javascript.js
@@ -2,6 +2,8 @@
 var a = 5;
 var b = a + 2;
 var z = a ** b;
+var p1 = (p1 || 0); //initializig the value with p1, or if not defined zero
+var p2 = (p2 || 4); //initializig the value with p1, or if not defined four
 var fullName = {firstName:"Peter", lastName:"Parker"}; // Object
 var clock = "way to late";
 var ingredients = ["banana", "nutbutter","milk"];
@@ -10,6 +12,17 @@ var ingredients = ["banana", "nutbutter","milk"];
 function displayNone() {
     document.getElementById("button").style.display = "none";
 }
+function boolean() {
+    document.getElementById("count").innerHTML = a == b;
+}
+
+function addition(p1, p2) {
+    return p1 + p2;
+}
+function countAddition (p1, p2) {
+    document.getElementById("count").innerHTML = addition(p1, p2);
+}
+
 function changeFontSize() {
     document.getElementById("date").style.fontSize = "60px"; 
     document.getElementById("date").style.color = "black";   
@@ -23,9 +36,7 @@ function alertWindow() {
 function exponentialOutput() {
     document.getElementById("body").innerHTML = fullName.firstName + " " + fullName.lastName;
 }
-function boolean() {
-    document.getElementById("count").innerHTML = a == b;
-}
+
 
 // do addition (no funtion necessary btw)
     document.getElementById("count").innerHTML = + z;
