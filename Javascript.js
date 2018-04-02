4aaca4942f3aa9fa5d3aaf1b80a63dc6ec16647b
basti <sebastian.neumair@gmail.com>
2018-04-02 16:53:22 +0200

    enter

--- Javascript.js
+++ Javascript.js
@@ -6,7 +6,12 @@ var p1 = (p1 || 0); //initializig the value with p1, or if not defined zero
 var p2 = (p2 || 4); //initializig the value with p1, or if not defined four
 var fullName = {firstName:"Peter", lastName:"Parker"}; // Object
 var clock = "way to late";
-var ingredients = ["banana", "nutbutter","milk"];
+var ingredients = ["banana", "nutbutter","milk"]; //ingredients is an array
+var shape = ["round", "edgy", "quadratic"]; // shapes is an array as well
+var pancakes = {ingredients:"banana",ingredients:"milk", shape:"round"}; // pancakes is an object described by 
+
+//
+document.getElementById("valueAdded").innerHTML = clock;
 
 //only active when function is called upon
 function displayNone() {
@@ -25,7 +30,9 @@ function countAddition (p1, p2) {
 
 function changeFontSize() {
     document.getElementById("date").style.fontSize = "60px"; 
-    document.getElementById("date").style.color = "black";   
+    document.getElementById("date").style.color = "black";
+    document.getElementById("subline").style.marginLeft = "20px";
+    document.getElementById("subline").style.fontSize = "50px";   
 }
 function deleteAll() {
     document.write("all Information has been deleted")
