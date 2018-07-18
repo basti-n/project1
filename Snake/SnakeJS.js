48f5ad0bca094246aa81277b853aed7ed0ece07c
basti <sebastian.neumair@gmail.com>
2018-07-18 22:27:27 +0200

    enter

---
+++ Snake/SnakeJS.js
@@ -0,0 +1,9 @@
+/**
+Constants 
+**/
+const CANVAS_BORDER_COLOUR = "black";
+const CANVAS_BACKGROUND_COLOUR = "white";
+
+// get the canvas element
+var canvas = document.getElementById("gameCanvas");
+
